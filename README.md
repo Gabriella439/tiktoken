4ac238c6ac8719810c9645cd4c25b300a1f8676c
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2024-08-31 11:55:29 -0700

    Update benchmarks

--- README.md
+++ README.md
@@ -10,20 +10,20 @@ encodings, but you can use it to consume encodings.  In particular, this comes
 in handy for prompt engineering where you want to use as much of the available
 prompt tokens as possible (which requires accurately counting tokens).
 
-Encoding speed is ≈7.5-9 MB/s on an M1 MacBook Pro (using only one core since
+Encoding speed is ≈2.6-3.1 MB/s on an M1 MacBook Pro (using only one core since
 this package does not yet support parallel tokenization):
 
 ```
 All
   Encode 10 MB of Wikipedia
-    r50k_base:   OK (3.33s)
-      1.102 s ±  43 ms
-    p50k_base:   OK (3.35s)
-      1.107 s ±  62 ms
-    p50k_edit:   OK (3.56s)
-      1.178 s ±  33 ms
-    cl100k_base: OK (3.96s)
-      1.308 s ±  29 ms
-    o200k_base:  OK (3.97s)
-      1.298 s ±  49 ms
+    r50k_base:   OK (23.88s)
+      3.356 s ± 151 ms
+    p50k_base:   OK (10.39s)
+      3.445 s ±  31 ms
+    p50k_edit:   OK (11.13s)
+      3.693 s ± 240 ms
+    cl100k_base: OK (11.16s)
+      3.685 s ± 143 ms
+    o200k_base:  OK (11.01s)
+      3.648 s ± 134 ms
 ```
--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -9,25 +9,20 @@ import Tiktoken (Encoding)
 import qualified Control.DeepSeq as DeepSeq
 import qualified Control.Exception as Exception
 import qualified Data.ByteString as ByteString
-import qualified Paths_tiktoken as Paths
 import qualified Test.Tasty.Bench as Bench
 import qualified Tiktoken
 
 main :: IO ()
 main = do
-    dataDirectory <- Paths.getDataDir
-
-    let file = dataDirectory </> "wikipedia_10mb.txt"
-
     Bench.defaultMain
-        [ Bench.env (ByteString.readFile file) \text -> do
+        [ Bench.env (ByteString.readFile "tasty/sample.txt") \text -> do
             let benchmark name encoding = do
                     let loadEncoding = Exception.evaluate (DeepSeq.force encoding)
 
                     Bench.env loadEncoding \encoding -> do
                         Bench.bench name (Bench.nf (Tiktoken.toTokens encoding) text)
 
-            Bench.bgroup "Encode 10 MB of Wikipedia"
+            Bench.bgroup "Encode 10 MiB of Wikipedia"
                 [ benchmark "r50k_base" Tiktoken.r50k_base
                 , benchmark "p50k_base" Tiktoken.p50k_base
                 , benchmark "p50k_edit" Tiktoken.p50k_edit
