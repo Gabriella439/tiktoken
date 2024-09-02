6c9aa2d91f847b8d4f7413fa04b79f38e6f1d83e
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2024-09-01 20:32:25 -0700

    Add test coverage for rendering original tokens

--- tasty/Main.hs
+++ tasty/Main.hs
@@ -2,6 +2,8 @@
 
 module Main where
 
+import Data.Text (Text)
+
 import qualified Data.ByteString as ByteString
 import qualified Data.Foldable as Foldable
 import qualified Data.Text as Text
@@ -12,29 +14,40 @@ import qualified Tiktoken
 main :: IO ()
 main = do
     Tasty.defaultMain do
-        let actualTokens input encoding = do
+        let actual encode input encoding = do
                 bytes <- ByteString.readFile input
-                case Tiktoken.toRanks encoding bytes of
+                case encode encoding bytes of
                     Nothing     -> fail "Encoding failed"
                     Just tokens -> return tokens
 
-        let normalTokens  = actualTokens "tasty/sample/input.txt"
-        let specialTokens = actualTokens "tasty/special/input.txt"
+        let normalRanks  = actual Tiktoken.toRanks "tasty/sample/input.txt"
+        let specialRanks = actual Tiktoken.toRanks "tasty/special/input.txt"
 
-        let render = Text.unlines . map (Text.pack . show) . Foldable.toList
+        let normalTokens =
+                actual Tiktoken.toTokens "tasty/tokenization/input.txt"
+
+        let render :: Show a => [a] -> Text
+            render = Text.unlines . map (Text.pack . show) . Foldable.toList
                 
         Tasty.testGroup "tests"
             [ Tasty.testGroup "golden"
-                [ Silver.goldenVsAction "r50k_base" "tasty/sample/r50k_base.golden" (normalTokens Tiktoken.r50k_base) render
-                , Silver.goldenVsAction "p50k_base" "tasty/sample/p50k_base.golden" (normalTokens Tiktoken.p50k_base) render
-                , Silver.goldenVsAction "cl100k_base" "tasty/sample/cl100k_base.golden" (normalTokens Tiktoken.cl100k_base) render
-                , Silver.goldenVsAction "o200k_base" "tasty/sample/o200k_base.golden" (normalTokens Tiktoken.o200k_base) render
+                [ Silver.goldenVsAction "r50k_base" "tasty/sample/r50k_base.golden" (normalRanks Tiktoken.r50k_base) render
+                , Silver.goldenVsAction "p50k_base" "tasty/sample/p50k_base.golden" (normalRanks Tiktoken.p50k_base) render
+                , Silver.goldenVsAction "cl100k_base" "tasty/sample/cl100k_base.golden" (normalRanks Tiktoken.cl100k_base) render
+                , Silver.goldenVsAction "o200k_base" "tasty/sample/o200k_base.golden" (normalRanks Tiktoken.o200k_base) render
                 ]
             , Tasty.testGroup "special tokens"
-                [ Silver.goldenVsAction "r50k_base" "tasty/special/r50k_base.golden" (specialTokens Tiktoken.r50k_base) render
-                , Silver.goldenVsAction "p50k_base" "tasty/special/p50k_base.golden" (specialTokens Tiktoken.p50k_base) render
-                , Silver.goldenVsAction "p50k_edit" "tasty/special/p50k_edit.golden" (specialTokens Tiktoken.p50k_edit) render
-                , Silver.goldenVsAction "cl100k_base" "tasty/special/cl100k_base.golden" (specialTokens Tiktoken.cl100k_base) render
-                , Silver.goldenVsAction "o200k_base" "tasty/special/o200k_base.golden" (specialTokens Tiktoken.o200k_base) render
+                [ Silver.goldenVsAction "r50k_base" "tasty/special/r50k_base.golden" (specialRanks Tiktoken.r50k_base) render
+                , Silver.goldenVsAction "p50k_base" "tasty/special/p50k_base.golden" (specialRanks Tiktoken.p50k_base) render
+                , Silver.goldenVsAction "p50k_edit" "tasty/special/p50k_edit.golden" (specialRanks Tiktoken.p50k_edit) render
+                , Silver.goldenVsAction "cl100k_base" "tasty/special/cl100k_base.golden" (specialRanks Tiktoken.cl100k_base) render
+                , Silver.goldenVsAction "o200k_base" "tasty/special/o200k_base.golden" (specialRanks Tiktoken.o200k_base) render
                 ]
+            , Tasty.testGroup "tokenization"
+                [ Silver.goldenVsAction "r50k_base" "tasty/tokenization/r50k_base.golden" (normalTokens Tiktoken.r50k_base) render
+                , Silver.goldenVsAction "p50k_base" "tasty/tokenization/p50k_base.golden" (normalTokens Tiktoken.p50k_base) render
+                , Silver.goldenVsAction "cl100k_base" "tasty/tokenization/cl100k_base.golden" (normalTokens Tiktoken.cl100k_base) render
+                , Silver.goldenVsAction "o200k_base" "tasty/tokenization/o200k_base.golden" (normalTokens Tiktoken.o200k_base) render
+                ]
+
             ]
