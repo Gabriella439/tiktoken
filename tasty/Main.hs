4228ea7488bfe69d1ceacb6a30b2ab38ac3b5619
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2024-09-01 20:21:19 -0700

    Add test coverage for special tokens

--- tasty/Main.hs
+++ tasty/Main.hs
@@ -12,17 +12,29 @@ import qualified Tiktoken
 main :: IO ()
 main = do
     Tasty.defaultMain do
-        let actualTokens encoding = do
-                bytes <- ByteString.readFile "tasty/sample.txt"
+        let actualTokens input encoding = do
+                bytes <- ByteString.readFile input
                 case Tiktoken.toRanks encoding bytes of
                     Nothing     -> fail "Encoding failed"
                     Just tokens -> return tokens
 
+        let normalTokens  = actualTokens "tasty/sample/input.txt"
+        let specialTokens = actualTokens "tasty/special/input.txt"
+
         let render = Text.unlines . map (Text.pack . show) . Foldable.toList
                 
-        Tasty.testGroup "golden"
-            [ Silver.goldenVsAction "r50k" "tasty/r50k_base.golden" (actualTokens Tiktoken.r50k_base) render
-            , Silver.goldenVsAction "p50k" "tasty/p50k_base.golden" (actualTokens Tiktoken.p50k_base) render
-            , Silver.goldenVsAction "cl100k" "tasty/cl100k_base.golden" (actualTokens Tiktoken.cl100k_base) render
-            , Silver.goldenVsAction "o200k" "tasty/o200k_base.golden" (actualTokens Tiktoken.o200k_base) render
+        Tasty.testGroup "tests"
+            [ Tasty.testGroup "golden"
+                [ Silver.goldenVsAction "r50k_base" "tasty/sample/r50k_base.golden" (normalTokens Tiktoken.r50k_base) render
+                , Silver.goldenVsAction "p50k_base" "tasty/sample/p50k_base.golden" (normalTokens Tiktoken.p50k_base) render
+                , Silver.goldenVsAction "cl100k_base" "tasty/sample/cl100k_base.golden" (normalTokens Tiktoken.cl100k_base) render
+                , Silver.goldenVsAction "o200k_base" "tasty/sample/o200k_base.golden" (normalTokens Tiktoken.o200k_base) render
+                ]
+            , Tasty.testGroup "special tokens"
+                [ Silver.goldenVsAction "r50k_base" "tasty/special/r50k_base.golden" (specialTokens Tiktoken.r50k_base) render
+                , Silver.goldenVsAction "p50k_base" "tasty/special/p50k_base.golden" (specialTokens Tiktoken.p50k_base) render
+                , Silver.goldenVsAction "p50k_edit" "tasty/special/p50k_edit.golden" (specialTokens Tiktoken.p50k_edit) render
+                , Silver.goldenVsAction "cl100k_base" "tasty/special/cl100k_base.golden" (specialTokens Tiktoken.cl100k_base) render
+                , Silver.goldenVsAction "o200k_base" "tasty/special/o200k_base.golden" (specialTokens Tiktoken.o200k_base) render
+                ]
             ]
